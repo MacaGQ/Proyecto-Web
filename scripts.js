5f40c235c670bdd18cfca76a38a9033a34d66bdc
MacaGQ <macarenadgq@gmail.com>
2022-10-26 13:03:30 -0300

    Arregle el menu hamburguesa 2

--- scripts.js
+++ scripts.js
@@ -9,11 +9,11 @@ function animacion() {
 
 function desplegarMenu() {
     let x = document.querySelector("nav ul").style;
-    if (x.transform === "translateX(120%)") {
-        x.transform = "translateX(0)";
+    if (x.transform === "translateX(0%)") {
+        x.transform = "translateX(120%)";
         x.transition = "0.5s ease-in"
     } else {
-        x.transform = "translateX(120%)";
+        x.transform = "translateX(0%)";
         x.transition = "0.5s ease-in"
     }
 }
